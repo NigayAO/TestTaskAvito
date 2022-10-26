efb24489ebbebd4611a413349af440db443ddbfc
Alik <Nigay_AO@bk.ru>
2022-10-26 15:51:11 +0600

    Setup initial project Remove storyboards Group files to folders

--- TestTaskAvito/SupportingFiles/AppDelegate.swift
+++ TestTaskAvito/SupportingFiles/AppDelegate.swift
@@ -10,10 +10,18 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+        } else {
+            window = UIWindow(frame: .zero)
+            
+            let navigationVC = UINavigationController(rootViewController: MainViewController())
+            window?.rootViewController = navigationVC
+            
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
