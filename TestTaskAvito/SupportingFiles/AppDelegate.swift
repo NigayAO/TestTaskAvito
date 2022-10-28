24ae45aced8ce85b8b964df8c85ac4cf678de421
Alik <Nigay_AO@bk.ru>
2022-10-29 00:50:32 +0600

    Final version App

--- TestTaskAvito/SupportingFiles/AppDelegate.swift
+++ TestTaskAvito/SupportingFiles/AppDelegate.swift
@@ -10,21 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 13, *) {
-        } else {
-            window = UIWindow(frame: .zero)
-            
-            let navigationVC = UINavigationController(rootViewController: MainViewController())
-            window?.rootViewController = navigationVC
-            
-            window?.makeKeyAndVisible()
-        }
-        
-//        NetworkMonitor.shared.startMonitoring()
-        
         return true
     }
 }
